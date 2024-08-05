06d5803e209a00db3d15caa3354320b257b7abaf
Kay9Unit <32242079+Kay9Unit@users.noreply.github.com>
2024-08-05 11:53:32 -0400

    init firebase on client only

--- firebase.js
+++ firebase.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -17,9 +16,12 @@ const firebaseConfig = {
   measurementId: "G-SXGE6FT8W6"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const firestore = getFirestore(app);
+let app
+let firestore
+
+if (typeof window !== 'undefined' && !getApps().length) {
+  app = initializeApp(firebaseConfig);
+  firestore = getFirestore(app);
+}
 
 export { firestore };
